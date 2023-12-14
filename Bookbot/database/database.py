d03d542c814b097a226e1c39d4fb8ef3c42c2ac7
AnastasiyaAuchM <129757987+AnastasiyaAuchM@users.noreply.github.com>
2023-12-14 22:42:30 +0400

    Delete Bookbot directory

--- Bookbot/database/database.py
+++
@@ -1,6 +0,0 @@
-# Create a template for the filling a users dictionary 
-user_dict_template: dict = {'page': 1,
-                            'bookmarks': set()}
-
-# Initialize the database
-users_db: dict = {}
